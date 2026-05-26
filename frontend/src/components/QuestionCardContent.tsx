accc6f9fda12c2fd731695aef20fa81ec541aff6
erik <erik.kletzin@gmail.com>
2026-05-26 20:47:36 +0200

    chore: remove websocket logic, because we are centralizing it with #29

--- frontend/src/components/QuestionCardContent.tsx
+++ frontend/src/components/QuestionCardContent.tsx
@@ -1,26 +1,16 @@
 // frontend/src/compontents/QuestionCardContent.tsx
 
 import type { JSX } from "react"
-// import { useContext } from "react"
 
 import { toast } from "sonner"
 import QuestionHeader from "@/components/QuestionHeader"
 import QuestionContainer from "@/components/QuestionContainer"
 import AnswerOption from "@/components/AnswerOption"
 
-// import { GameSessionContext } from "@/context/GameSessionContext" // COMMENTED OUT FOR TESTING
-// import useQuestion from "@/hooks/useQuestion" // COMMENTED OUT FOR TESTING
-
 const ICONS = ["▲", "◆", "●", "■", "◯", "◆"]
 const COLORS = ["#2d4cc9", "#ffa602", "#11c8d4", "#ff4949", "#28c28b", "#8b5cf6"]
 
 export default function QuestionCardContent(): JSX.Element {
-    // const sessionContext = useContext(GameSessionContext) // COMMENTED OUT FOR TESTING
-    // const socketService = sessionContext?.socketService ?? null // COMMENTED OUT FOR TESTING
-
-    // const { question, remainingTime, selectedAnswer, sendAnswer } = useQuestion(socketService) // COMMENTED OUT FOR TESTING
-
-    // TODO: REMOVE MOCK DATA AFTER TESTING
     const remainingTime = 12
     const selectedAnswer = "0"
     const sendAnswer = (id: string) => {
@@ -44,8 +34,6 @@ export default function QuestionCardContent(): JSX.Element {
             { id: "opt_4", text: "They bought a mechanical keyboard" },
         ],
     }
-
-    // MOCK DATA END
 
     return (
         <div className="bg-background text-foreground min-h-screen px-4 py-8">
--- frontend/src/pages/SingleChoiceQuestion.tsx
+++ frontend/src/pages/SingleChoiceQuestion.tsx
@@ -1,17 +1,9 @@
 // frontend/src/pages/SingleChoiceQuestion.tsx
 
 import type { JSX } from "react"
-import { useParams } from "react-router"
 
 import QuestionCardContent from "../components/QuestionCardContent"
-import GameSessionProvider from "@/context/GameSessionProvider"
 
 export default function SingleChoiceQuestion(): JSX.Element {
-    const { code } = useParams()
-
-    return (
-        <GameSessionProvider code={code}>
-            <QuestionCardContent />
-        </GameSessionProvider>
-    )
+    return <QuestionCardContent />
 }
