dac47f59e03d729fc1ec593e9d91ea35df5b3101
Noel Hoppe <noel.hoppe@gmx.de>
2026-06-15 20:32:25 +0200

    refactor(types): mark types as deprecated

--- frontend/src/types/question.ts
+++ frontend/src/types/question.ts
@@ -1,8 +1,14 @@
 import type { QuestionApiResponse } from "@/api/questions/types/question.api.ts"
 
+/**
+ * @deprecated
+ */
 export type QuestionOption =
     Extract<QuestionApiResponse["options"], unknown[]> extends (infer Option)[] ? Option : never
 
+/**
+ * @deprecated
+ */
 export type Question = Omit<QuestionApiResponse, "options" | "created" | "modified"> & {
     options: QuestionOption[]
 }
